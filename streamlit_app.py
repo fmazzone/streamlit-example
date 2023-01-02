20093df232079b18f5e6cbfa8f01eba1c2742b34
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 22:38:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,12 @@ st.image('https://www.analyticssteps.com/backend/media/thumbnail/6006173/6278986
   
   # ------ layout setting---------------------------
 window_selection_c = st.sidebar.container() # create an empty container in the sidebar
-window_selection_c.markdown("## Insights") # add a title to the sidebar container
-sub_columns = window_selection_c.columns(2) #Split the container into two columns for start and end date
-
+window_selection_c.markdown("## _Dati_") # add a title to the sidebar container
+sub_columns = window_selection_c.columns(2) #Split the container into two columns 
 # ----------Time window selection-----------------
 YESTERDAY=datetime.date.today()-datetime.timedelta(days=1)
 label="Giorno per la predizione"
-sub_columns[0].date_input(label,datetime.date(2019, 7, 6))
+sub_columns[0].date_input(label,datetime.date(2018, 1, 1))
+labelq="Città"
+sub_columns[1].text_input('')
 
