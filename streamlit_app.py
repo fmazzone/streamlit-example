4e2a6d5e6ec4f89311a723b0f63bed725c0fb134
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 17:48:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # Required Libraries
 import streamlit as st
-import matplotlib
 from datetime import datetime
-from API import owm
-from pyowm.commons.exceptions import NotFoundError
 
 # Streamlit Display
 st.set_page_config(layout="centered")
