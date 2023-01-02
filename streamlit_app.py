c7b4aac66edea948a341b34e443dda486f04b223
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 23:11:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ import datetime
 
 st.set_page_config(layout="centered", initial_sidebar_state="expanded")
 st.title('_:blue[Weather Forecast]_')
-st.image('https://www.analyticssteps.com/backend/media/thumbnail/6006173/6278986_1571298721_Weather_Forecoast_Graphics.jpg')
-
   
   # ------ layout setting---------------------------
 window_selection_c = st.sidebar.container() # create an empty container in the sidebar
@@ -14,6 +12,7 @@ window_selection_c.markdown("## _Dati_") # add a title to the sidebar container
 sub_columns = window_selection_c.columns(2) #Split the container into two columns 
 
 sub_columns[0].date_input("Data per la predizione",datetime.date(2018, 1, 1))
+sub_columns[0].text_input("Città per la predizione")
 
 col1, col2, col3,col4,col5,col6,col7,col8, = st.columns(8)
 
