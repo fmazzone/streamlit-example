27ec044c1e251b371448de630c6535a89656b2c9
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 19:00:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,7 @@ st.title('Weather Forecasting Project')
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # Can be used wherever a "file-like" object is accepted:
+    
     dataframe = pd.read_csv(uploaded_file)
-    st.dataframe(dataframe)
+    
+    st.show(dataframe)
