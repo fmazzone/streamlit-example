ff9ac44d18e757ae4413488a29c9d8859a50eaa4
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 19:45:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import datetime 
 
-st.title('Weather Forecasting Project')
+st.set_page_config(page_title='Weather Forecasting Project', page_icon='https://is2-ssl.mzstatic.com/image/thumb/Purple122/v4/b2/d0/ef/b2d0efa6-060a-76a5-d169-83ca6011c193/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png',layout="wide", initial_sidebar_state="expanded")
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    
-    dataframe = pd.read_csv(uploaded_file)
-    
-    st.show(dataframe)
+
