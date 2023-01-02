da55a948a78376b7b38dcabf4fe65d17d73040aa
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 23:34:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ st.title('_:blue[Weather Forecast]_')
 
 
   # ------ layout setting---------------------------
-'''window_selection_c = st.sidebar.container() # create an empty container in the sidebar
-window_selection_c.markdown("## _Dati_") # add a title to the sidebar container
-sub_columns = window_selection_c.columns(2) #Split the container into two columns 
+#window_selection_c = st.sidebar.container() # create an empty container in the sidebar
+#window_selection_c.markdown("## _Dati_") # add a title to the sidebar container
+#sub_columns = window_selection_c.columns(2) #Split the container into two columns 
 
-sub_columns[0].date_input("Data per la predizione",datetime.date(2018, 1, 1))
-sub_columns[0].text_input("Città per la predizione")'''
+#sub_columns[0].date_input("Data per la predizione",datetime.date(2018, 1, 1))
+#sub_columns[0].text_input("Città per la predizione")
 
 col1, col2, col3,col4,col5,col6,col7,col8, = st.columns(8)
 
