00361cced5071a997b690784d16700c1d7767b7d
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 22:40:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,8 @@ st.image('https://www.analyticssteps.com/backend/media/thumbnail/6006173/6278986
   # ------ layout setting---------------------------
 window_selection_c = st.sidebar.container() # create an empty container in the sidebar
 window_selection_c.markdown("## _Dati_") # add a title to the sidebar container
-sub_columns = window_selection_c.columns(2) #Split the container into two columns 
+sub_columns = window_selection_c.row(2) #Split the container into two columns 
 # ----------Time window selection-----------------
-YESTERDAY=datetime.date.today()-datetime.timedelta(days=1)
 label="Giorno per la predizione"
 sub_columns[0].date_input(label,datetime.date(2018, 1, 1))
 sub_columns[1].text_input('Città')
