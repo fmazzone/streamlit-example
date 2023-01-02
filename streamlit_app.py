a2729d7d19513a12c61d69e591f0f95d4eff4035
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 19:45:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime 
 
+st.title('Stock forecast dashboard')
 st.set_page_config(page_title='Weather Forecasting Project', page_icon='https://is2-ssl.mzstatic.com/image/thumb/Purple122/v4/b2/d0/ef/b2d0efa6-060a-76a5-d169-83ca6011c193/AppIcon-0-0-1x_U007emarketing-0-0-0-10-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1200x630wa.png',layout="wide", initial_sidebar_state="expanded")
 
 
