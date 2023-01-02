317fc14ebb65ee989a9a69f2a28a2a2ec71991f9
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-02 19:52:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import datetime 
 
 st.title('Weather Forecast')
-st.set_page_config(page_title='Weather Forecasting Project', page_icon=':sun:',layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title='Weather Forecasting Project',layout="wide", initial_sidebar_state="expanded")
 
 st.image('https://www.analyticssteps.com/backend/media/thumbnail/6006173/6278986_1571298721_Weather_Forecoast_Graphics.jpg')
 
