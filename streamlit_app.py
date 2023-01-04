d0f0febd5900a69d0bb866ba58a29a49f7b42fcd
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-04 17:39:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,9 @@ sub_columns[0].text_input('Temperatura')
 sub_columns[0].text_input('vento')
 sub_columns[0].text_input('umidità')
 
-with sub_columns[0].expander("ddd"):
-      sub_columns[0].text_input('152')
-      sub_columns[0].text_input('125')
+with sub_columns[1].expander("ddd"):
+   sub_columns[0].text_input('152')
+   sub_columns[0].text_input('125')
    
 sub_columns[0].button('submit')
 
