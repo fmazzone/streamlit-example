373f524c57841d092023b1b78fb50cfcc50c788b
fmazzone <74185249+fmazzone@users.noreply.github.com>
2023-01-04 17:30:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,10 @@ sub_columns = window_selection_c.columns(2) #Split the container into two column
 
 #sub_columns[0].date_input("Data per la predizione",datetime.date(2018, 1, 1))
 #keyword = sub_columns[0].text_input("Città per la predizione")
-sub_columns[0].text_box('Temperatura')
-sub_columns[0].text_box('vento')
-sub_columns[0].text_box('umidità')
+sub_columns[0].text_input('Temperatura')
+sub_columns[0].text_input('vento')
+sub_columns[0].text_input('umidità')
+sub_columns[0].button('submit')
 
 col1, col2, col3,col4,col5,col6,col7, = st.columns(7)
 
